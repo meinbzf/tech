02d5f4c8104e9ff5a5ea03ecd2079ba6b86bd6fa
meinbzf <58031493+meinbzf@users.noreply.github.com>
2020-01-03 17:35:52 +0100

    Add files via upload

--- js/meineDatei.js
+++ js/meineDatei.js
@@ -1,22 +1,37 @@
+/* eslint-disable no-undef */
 "use strict";
 let aktuelleFrage = 0;
-const MAX_FRAGEN = 231;
-const korrekteAntwort = 'a';
+let MAX_FRAGEN = 0;
+let docTitle = document.title;
+if (docTitle.match("Luftrecht"))
+    MAX_FRAGEN = 195;
+if (docTitle.match("Meteorologie"))
+    MAX_FRAGEN = 170;
+if (docTitle.match("Navigation"))
+    MAX_FRAGEN = 111;
+if (docTitle.match("Navigations-Aufgabe"))
+    MAX_FRAGEN = 20;
+if (docTitle.match("Technik"))
+    MAX_FRAGEN = 231;
+if (docTitle.match("Menschliches"))
+    MAX_FRAGEN = 70;
+if (docTitle.match("Verhalten"))
+    MAX_FRAGEN = 127;
 
-verstecke(true);
+// 4 Antwortmöglichkeiten, auf die geklickt werden kann
+let radioA;
+let radioB;
+let radioC;
+let radioD;
 
-let myBtnPruefe = document.getElementById("pruefe");
-myBtnPruefe.addEventListener("click", setzeFarbe);
+let modusNurGespeicherteFragen = false;
 
+blaettern(true);
 
 function setzeFarbe() {
-    let radioA = document.getElementById(aktuelleFrage + "aRadio");
     let meinTextA = document.getElementById(aktuelleFrage + "a" + "Text");
-    let radioB = document.getElementById(aktuelleFrage + "bRadio");
     let meinTextB = document.getElementById(aktuelleFrage + "b" + "Text");
-    let radioC = document.getElementById(aktuelleFrage + "cRadio");
     let meinTextC = document.getElementById(aktuelleFrage + "c" + "Text");
-    let radioD = document.getElementById(aktuelleFrage + "dRadio");
     let meinTextD = document.getElementById(aktuelleFrage + "d" + "Text");
     meinTextA.style.backgroundColor = "white";
     meinTextB.style.backgroundColor = "white";
@@ -24,7 +39,7 @@ function setzeFarbe() {
     meinTextD.style.backgroundColor = "white";
     meinTextA.style.backgroundColor = "LightGreen";
     if (radioB.checked) {
-        meinTextB.style.backgroundColor = "red"; //LightCoral
+        meinTextB.style.backgroundColor = "red";
     }
     if (radioC.checked) {
         meinTextC.style.backgroundColor = "red";
@@ -32,35 +47,72 @@ function setzeFarbe() {
     if (radioD.checked) {
         meinTextD.style.backgroundColor = "red";
     }
-
-    /*
-    if (radioA.checked && korrekteAntwort == 'a') {
-        //meinTextA.innerHTML = "neu";
-        //meinTextA.style.color = "green";
-        meinTextA.style.backgroundColor = "LightGreen";
+    // Korrekt, dann automatisch weiterblättern
+    if (radioA.checked) {
+        // etwas Zeit verstreichen lassen
+        window.setTimeout(function () { blaettern(true); }, 1000);
     }
-    */
 }
+
 let myBtnGehezu = document.getElementById("gehezu");
 myBtnGehezu.addEventListener("click", setzeNummer);
-
 function setzeNummer() {
-    let meineEingabe = prompt("Bitte neue Nummer eingeben:", "123");
-    aktuelleFrage = Number(meineEingabe) - 1;
-    verstecke(true);
+    bootbox.prompt({
+        title: "Bitte neue Nummer eingeben:",
+        value: "123",
+        callback: function (result) {
+            if (result === null) {
+                //keine Aktion
+            } else {
+                aktuelleFrage = Number(result) - 1;
+                blaettern(true);
+            }
+        }
+    });
+    //let meineEingabe = prompt("Bitte neue Nummer eingeben:", "123");
+    //aktuelleFrage = Number(meineEingabe) - 1;
+    //blaettern(true);
 }
 
 let myBtnWeiter = document.getElementById("weiter");
-myBtnWeiter.addEventListener("click", verstecke.bind(this, true));
+myBtnWeiter.addEventListener("click", blaettern.bind(this, true));
 let myBtnZurueck = document.getElementById("zurueck");
-myBtnZurueck.addEventListener("click", verstecke.bind(this, false));
+myBtnZurueck.addEventListener("click", blaettern.bind(this, false));
 
 // weiter gibt an, ob vorwärts oder rückwärts geblättert wird
-function verstecke(weiter) {
-    if (weiter)
-        aktuelleFrage++;
-    else
-        aktuelleFrage--;
+function blaettern(weiter) {
+    if (!modusNurGespeicherteFragen) {
+        if (weiter)
+            aktuelleFrage++;
+        else
+            aktuelleFrage--;
+    }
+    // Nur gespeicherte Fragen anzeigen
+    if (modusNurGespeicherteFragen) {
+        let myData = localStorage.getItem(docTitle);
+        let arrayGesplittet = myData.split(",");
+        let index = arrayGesplittet.indexOf(aktuelleFrage.toString());
+        //alert("Index ist: " + index + " Array-Länge: " + arrayGesplittet.length);
+        if (aktuelleFrage == -1)
+            aktuelleFrage = arrayGesplittet[0]; //erstes Element anzeigen
+        else {
+            if (weiter && index + 1 < arrayGesplittet.length) {
+                aktuelleFrage = arrayGesplittet[index + 1];
+            }
+            if (!weiter && index > 0) {
+                aktuelleFrage = arrayGesplittet[index - 1];
+            }
+        }
+    }
+
+    radioA = document.getElementById(aktuelleFrage + "aRadio");
+    radioA.addEventListener("click", setzeFarbe);
+    radioB = document.getElementById(aktuelleFrage + "bRadio");
+    radioB.addEventListener("click", setzeFarbe);
+    radioC = document.getElementById(aktuelleFrage + "cRadio");
+    radioC.addEventListener("click", setzeFarbe);
+    radioD = document.getElementById(aktuelleFrage + "dRadio");
+    radioD.addEventListener("click", setzeFarbe);
 
     anzeigeAnzahl();
     for (let i = 1; i <= MAX_FRAGEN; i++) {
@@ -68,6 +120,7 @@ function verstecke(weiter) {
             document.getElementById(i).style.display = 'block';
         }
         else {
+            //nicht anzeigen
             document.getElementById(i).style.display = 'none';
         }
     }
@@ -98,4 +151,61 @@ function anzeigeAnzahl() {
     let meineAnzeige = document.getElementById("anzeigeAnzahl");
     meineAnzeige.innerHTML = aktuelleFrage + "/" + MAX_FRAGEN;
 }
-// Hinweis Firefox: Einstellungen: Datenschutz: Private Daten beim Beenden löschen: Gespeicherte Formulardaten
+
+let myBtnSpeichern = document.getElementById("speichern");
+myBtnSpeichern.addEventListener("click", speichern);
+function speichern() {
+    let myData = localStorage.getItem(docTitle);
+    if (myData == null) {
+        localStorage.setItem(docTitle, aktuelleFrage);
+    }
+    else {
+        let arrayGesplittet = myData.split(",");
+        if (arrayGesplittet.indexOf(aktuelleFrage.toString()) == -1) {
+            // noch nicht gespeicherte Frage kommt hinzu
+            arrayGesplittet.push(aktuelleFrage);
+            arrayGesplittet.sort(function (a, b) {
+                return a - b; // numerisch sortieren, nicht alphabetisch
+            });
+            localStorage.setItem(docTitle, arrayGesplittet);
+            // alert(arrayGesplittet + " Index of: " + arrayGesplittet.indexOf(aktuelleFrage.toString()) + " localStorage: " + localStorage.getItem(docTitle));
+        }
+    }
+
+    //localStorage.setItem('myCat', 'Tom');
+    //let cat = localStorage.getItem('myCat');
+    //localStorage.removeItem('myCat');
+    //localStorage.clear(); //alles löschen
+    //alert(localStorage.getItem(docTitle));
+}
+
+let myBtnNurGespeicherte = document.getElementById("gespeichert");
+myBtnNurGespeicherte.addEventListener("click", nurGespeicherte);
+function nurGespeicherte() {
+    let myData = localStorage.getItem(docTitle);
+    let anzeigeText = "Gespeicherte Fragen für " + docTitle + ": " + myData;
+    anzeigeText += "<br>Falls Daten nicht gespeichert werden, dann: ";
+    anzeigeText += "<br> - Firefox: Einstellungen, Datenschutz, Cookies erlauben, aber nicht zur Verfolgung";
+    anzeigeText += "<br> - Chrome: Einstellungen, Datenschutz, Browserdaten löschen, Cookies und Websitedaten zulassen";
+    anzeigeText += "<br>Zusatzinfo zur Verwaltung des lokalen Speichers im Browser unter Win10: F12, dann Application tab (Web-Speicher). Storage section: Local Storage";
+    bootbox.alert(anzeigeText);
+    aktuelleFrage = -1; //später erstes Element anzeigen
+    modusNurGespeicherteFragen = true;
+    blaettern(true);
+}
+
+let myBtnLoeschen = document.getElementById("loeschen");
+myBtnLoeschen.addEventListener("click", loescheListe);
+function loescheListe() {
+    bootbox.confirm("Sollen alle gespeicherten Fragen der Kategorie " + docTitle + " aus der Merkliste gelöscht werden?", function (result) {
+        //alert("Bootbox Ergebnis" + result);
+        if (result == true) {
+            localStorage.removeItem(docTitle);
+            let myData = localStorage.getItem(docTitle);
+            bootbox.alert("Gespeicherte Fragen: " + myData);
+            modusNurGespeicherteFragen = false;
+            aktuelleFrage = 0;
+            blaettern(true);
+        }
+    });
+}
